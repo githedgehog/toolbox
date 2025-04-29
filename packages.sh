438d2d7cae071ff809b08e3e15945b5d224e05aa
Sergei Lukianov <me@slukjanov.name>
2025-04-28 23:42:57 -0700

    Actually install kubectl to a bin
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- packages.sh
+++ packages.sh
@@ -31,4 +31,6 @@ apt-get update -y \
     && apt-get autoremove --yes \
     && rm -rf /var/lib/apt/lists/*
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+    && chmod +x kubectl \
+    && mv kubectl /usr/local/bin/
