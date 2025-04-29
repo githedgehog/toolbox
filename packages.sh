3d4691f1a5c4db2bd65321ec41a44d500fe1caf7
Sergei Lukianov <me@slukjanov.name>
2025-04-28 23:26:51 -0700

    Extend with few more toolsk and kubectl
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- packages.sh
+++ packages.sh
@@ -17,6 +17,10 @@ APT_PACKAGES=(
     tcpdump
     traceroute
     wget
+    vim
+    curl
+    wget
+    pciutils
 )
 
 apt-get update -y \
@@ -25,3 +29,5 @@ apt-get update -y \
     && apt-get clean autoclean \
     && apt-get autoremove --yes \
     && rm -rf /var/lib/apt/lists/*
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
